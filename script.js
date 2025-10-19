3c9a6977ce5e7cce8eecef4f632845de9a8c7455
Zainab357 <202300925@student.polytechnic.bh>
2025-10-19 17:10:41 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+ document.getElementById("contact form").onsubmit=(){alert("your inquiry has been submitted.");return false;};
+decument.getElementByTagName("tr")[1].onclick=function(){alert("Cookies Sold Out");};
